74aaa24ba9c303658ed4144032d9dbc7168f236c
VickyyGerb <vickyag2007@gmail.com>
2025-08-12 11:20:27 -0300

    olu

--- tests/testOctus.spec.ts
+++ tests/testOctus.spec.ts
@@ -3,7 +3,11 @@ import { test, expect } from "@playwright/test";
 test("Octus", async ({ page }) => {
   await page.goto("https://dev.octus.com.ar/");
   await page.getByRole("link", { name: "Iniciar sesión" }).click();
+
   await page
     .getByRole("textbox", { name: "E-mail" })
     .fill("colegiocba@octus.com.ar");
+  await page.locator('input[id="userPass"]').fill("pass");
+  await page.getByRole("button", { name: "Ingresar" }).click();
+  await page.pause();
 });
